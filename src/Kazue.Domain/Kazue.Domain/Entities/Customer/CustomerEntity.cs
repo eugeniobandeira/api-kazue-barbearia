f7b7f01945ffb1a1319b1cba606e1b4b7e1b9c3e
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-08 16:44:38 -0300

    feat: creating main entities

---
+++ src/Kazue.Domain/Kazue.Domain/Entities/Customer/CustomerEntity.cs
@@ -0,0 +1,10 @@
+﻿using Kazue.Domain.Entities.Person;
+using Kazue.Domain.Helper;
+
+namespace Kazue.Domain.Entities.Customer;
+
+public class CustomerEntity : PersonEntity
+{
+    public string Role { get; set; } = RolesHelper.USER;
+    public Guid CustomerGuid  { get; set; }
+}
---
+++ src/Kazue.Domain/Kazue.Domain/Entities/Person/PersonEntity.cs
@@ -0,0 +1,15 @@
+﻿using Kazue.Domain.Helper;
+
+namespace Kazue.Domain.Entities.Person;
+
+public abstract class PersonEntity
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string? Nickname { get; set; } = string.Empty;
+    public string? Email { get; set; } = string.Empty;
+    public string? Phone { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public DateTime DateOfRegistry { get; set; }
+}
