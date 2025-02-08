9129d6852611f8ced9cbd365091d0ed80355b279
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-08 16:46:31 -0300

    feat: registered person and ListApiResponse

---
+++ src/Kazue.Domain/Kazue.Domain/Response/Person/RegisteredPersonResponse.cs
@@ -0,0 +1,9 @@
+﻿using Kazue.Domain.Helper;
+
+namespace Kazue.Domain.Response.Person;
+
+public abstract class PersonResponse
+{
+    public string NM_PERSON { get; set; } = string.Empty;
+    public string DS_TOKEN { get; set; } = string.Empty;
+}
