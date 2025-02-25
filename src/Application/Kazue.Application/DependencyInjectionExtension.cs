79ab5c9eb52d7a88a3dcd9a9e40a82728ee9ce73
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-24 21:43:19 -0300

    feat: application bootstrap

---
+++ src/Application/Kazue.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Kazue.Application.UseCases.Barber.Create;
+using Kazue.Application.UseCases.Barber.GetById;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kazue.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddUseCases(services);
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<ICreateBarberUseCase, CreateBarberUseCase>();
+        services.AddScoped<IGetBarberByIdUseCase, GetBarberByIdUseCase>();
+    }
+
+}
