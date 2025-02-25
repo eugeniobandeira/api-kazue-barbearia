e3e3aa2c2a93a5662101b9b384809e9d7ded8f26
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-24 21:42:21 -0300

    feat: create barber use case

---
+++ src/Application/Kazue.Application/UseCases/Barber/Create/ICreateBarberUseCase.cs
@@ -0,0 +1,8 @@
+﻿using Kazue.Domain.Request.Barber;
+using Kazue.Domain.Response.Person;
+
+namespace Kazue.Application.UseCases.Barber.Create;
+public interface ICreateBarberUseCase
+{
+    Task<RegisteredPersonResponse> ExecuteAsync(CreateBarberRequest req);
+}
