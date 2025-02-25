e3e2d422cfe671c9a93db39bf46e8c8dc88c27a5
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-24 21:45:49 -0300

    feat: barber parameters

---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Barber/BarberParameters.cs
@@ -0,0 +1,37 @@
+﻿using Kazue.Domain.Request.Barber;
+using Kazue.Infrastructure.Helpers;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Kazue.Infrastructure.Parameters.Barber;
+
+public class BarberParameters : 
+    IBarberParameter
+{
+    public MySqlDynamicParameters CreateParameters(CreateBarberRequest req)
+    {
+        var parameters = new MySqlDynamicParameters();
+
+        parameters.Add("P_DS_NAME", MySqlDbType.String, ParameterDirection.Input, req.Name);
+        parameters.Add("P_DS_SURNAME", MySqlDbType.String, ParameterDirection.Input, req.Surname);
+        parameters.Add("P_DS_NICKNAME", MySqlDbType.String, ParameterDirection.Input, req.Nickname);
+        parameters.Add("P_DS_EMAIL", MySqlDbType.String, ParameterDirection.Input, req.Email);
+        parameters.Add("P_DS_PHONE", MySqlDbType.String, ParameterDirection.Input, req.Phone);
+        parameters.Add("P_DS_ROLE", MySqlDbType.String, ParameterDirection.Input, req.Role);
+
+        parameters.Add("P_DS_PASSWORD", MySqlDbType.String, ParameterDirection.Input, req.Password);
+
+        parameters.Add("P_DT_BIRTH", MySqlDbType.DateTime, direction: ParameterDirection.Input, req.DateOfBirth);
+
+        return parameters;
+    }
+
+    public MySqlDynamicParameters GetByIdParameters(long id)
+    {
+        var parameters = new MySqlDynamicParameters();
+
+        parameters.Add("P_ID_BARBER", MySqlDbType.Int32, ParameterDirection.Input, id);
+
+        return parameters;
+    }
+}
---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Barber/IBarberParameter.cs
@@ -0,0 +1,9 @@
+﻿using Kazue.Domain.Request.Barber;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Barber;
+public interface IBarberParameter
+{
+    MySqlDynamicParameters CreateParameters(CreateBarberRequest req);
+    MySqlDynamicParameters GetByIdParameters(long id);
+}
