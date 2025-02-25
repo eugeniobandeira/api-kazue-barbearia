86efe5df284363df1c489326a10a04add6f3c35e
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-24 21:42:36 -0300

    get barber by id use case

---
+++ src/Application/Kazue.Application/UseCases/Barber/GetById/GetBarberByIdUseCase.cs
@@ -0,0 +1,24 @@
+﻿using Kazue.Application.Adapter.Barber;
+using Kazue.Domain.Response.Person;
+using Kazue.Infrastructure.Repository.Barber;
+
+namespace Kazue.Application.UseCases.Barber.GetById;
+
+public class GetBarberByIdUseCase : IGetBarberByIdUseCase
+{
+    private readonly IBarberRepository _barberRepository;
+
+    public GetBarberByIdUseCase(IBarberRepository barberRepository)
+    {
+        _barberRepository = barberRepository;
+    }
+
+    public async Task<RegisteredPersonResponse> ExecuteAsync(long id)
+    {
+        var repositoryResponse = await _barberRepository.GetById(id);
+
+        var response = BarberAdapter.FromEntityToResponse(repositoryResponse);
+
+        return response;
+    }
+}
---
+++ src/Application/Kazue.Application/UseCases/Barber/GetById/IGetBarberByIdUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Kazue.Domain.Request.Barber;
+using Kazue.Domain.Response.Person;
+
+namespace Kazue.Application.UseCases.Barber.GetById;
+
+public interface IGetBarberByIdUseCase
+{
+    Task<RegisteredPersonResponse> ExecuteAsync(long id);
+}
