7f13e1f9266b3ef6ee061ec0bfb42d8226447564
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-24 21:46:16 -0300

    feat: barber repository

---
+++ src/Infrastructure/Kazue.Infrastructure/Queries/Barber/BarberQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Kazue.Infrastructure.Queries.Barber;
+
+public class BarberQuery
+{
+    public const string SP_CREATE_BARBER = "SP_CREATE_BARBER";
+
+    public const string SP_GET_BARBER_BY_ID = "SP_GET_BARBER_BY_ID";
+}
---
+++ src/Infrastructure/Kazue.Infrastructure/Repository/Barber/IBarberRepository.cs
@@ -0,0 +1,10 @@
+﻿using Kazue.Domain.Entities.Barber;
+using Kazue.Domain.Request.Barber;
+
+namespace Kazue.Infrastructure.Repository.Barber;
+
+public interface IBarberRepository
+{
+    Task<BarberEntity> Create(CreateBarberRequest req);
+    Task<BarberEntity?> GetById(long id);
+}
