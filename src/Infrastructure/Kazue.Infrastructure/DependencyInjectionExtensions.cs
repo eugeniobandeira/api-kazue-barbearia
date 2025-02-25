37dc1be32bda2a40d47192676fd798c0a215c67c
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-24 21:46:38 -0300

    feat: infrastructure bootstrap

---
+++ src/Infrastructure/Kazue.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Kazue.Domain.Interfaces.Connection;
+using Kazue.Infrastructure.Connection;
+using Kazue.Infrastructure.Parameters.Barber;
+using Kazue.Infrastructure.Repository.Barber;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kazue.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddRepositories(services);
+        AddParameters(services);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IBarberRepository, BarberRepository>();
+
+    }
+
+    private static void AddParameters(IServiceCollection services)
+    {
+        services.AddScoped<IKazueConnection, KazueConnection>();
+        services.AddScoped<IBarberParameter, BarberParameters>();
+    }
+}
