0cdab09d67461182daf0c165bcf6d50cdac40c9b
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-02-28 14:00:42 -0300

    updating user adapter

--- src/Application/Kazue.Application/Adapter/User/UserAdapter.cs
+++ src/Application/Kazue.Application/Adapter/User/UserAdapter.cs
@@ -1,24 +1,26 @@
-﻿using Kazue.Domain.Entities.Barber;
+﻿using Kazue.Domain.Entities.User;
 using Kazue.Domain.Response.Person;
 
-namespace Kazue.Application.Adapter.Barber;
+namespace Kazue.Application.Adapter.User;
 
-public static class BarberAdapter
+public static class UserAdapter
 {
-    public static RegisteredPersonResponse FromEntityToResponse(BarberEntity? entity)
+    public static UserResponse FromEntityToResponse(UserEntity? entity)
     {
         if (entity is null)
             throw new ArgumentNullException("Cannot adapt a null entity to response", nameof(entity));
 
-        var response = new RegisteredPersonResponse()
+        var response = new UserResponse()
         {
-            Id = entity.ID_BARBER,
-            Surname = entity.DS_ROLE,
+            Id = entity.ID_USER,
+            Surname = entity.DS_SURNAME,
             Nickname = entity.DS_NICKNAME,
             Email = entity.DS_EMAIL,
             Phone = entity.DS_PHONE,
             Name = entity.DS_NAME,
-            Status = entity.TP_STATUS,
+            Role = entity.DS_ROLE,
+            IdStatus = entity.ID_STATUS,
+            StatusDescription = entity.DS_STATUS,
             DateOfBirth = entity.DT_BIRTH,
             DateOfRegistry = entity.DT_REGISTRY
         };
