7d89520f70ccd7bcbcff26b8e4fd197585283ccc
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:06:57 -0300

    feat: queue parameter

---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Queue/IQueueParameter.cs
@@ -0,0 +1,11 @@
+﻿using Kazue.Domain.Request.Queue;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Queue;
+
+public interface IQueueParameter
+{
+    MySqlDynamicParameters CreateParameters(QueueRequest req);
+    MySqlDynamicParameters UpdateParameters(long id, QueueRequest req);
+    MySqlDynamicParameters GetByIdParameters(long id);
+}
---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Queue/QueueParameter.cs
@@ -0,0 +1,22 @@
+﻿using Kazue.Domain.Request.Queue;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Queue;
+
+public class QueueParameter : IQueueParameter
+{
+    public MySqlDynamicParameters CreateParameters(QueueRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MySqlDynamicParameters GetByIdParameters(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MySqlDynamicParameters UpdateParameters(long id, QueueRequest req)
+    {
+        throw new NotImplementedException();
+    }
+}
