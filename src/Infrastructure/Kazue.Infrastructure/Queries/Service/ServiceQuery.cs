1e3c22278d38e9b18fea0e00495251869c64c597
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:07:54 -0300

    feat: service query

---
+++ src/Infrastructure/Kazue.Infrastructure/Queries/Service/ServiceQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace Kazue.Infrastructure.Queries.Service;
+
+public class ServiceQuery
+{
+    public const string SERVICE_SP_CREATE = "SERVICE_SP_CREATE";
+
+    public const string SERVICE_SP_DELETE = "SERVICE_SP_DELETE";
+
+    public const string SERVICE_SP_GET = "SERVICE_SP_GET";
+
+    public const string SERVICE_SP_GET_BY_ID = "SERVICE_SP_GET_BY_ID";
+
+    public const string SERVICE_SP_UPDATE = "SERVICE_SP_UPDATE";
+}
