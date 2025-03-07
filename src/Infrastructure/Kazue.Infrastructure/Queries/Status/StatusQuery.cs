82b1982718982077e848e60473d42df35544b8ae
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:08:02 -0300

    feat: status query

---
+++ src/Infrastructure/Kazue.Infrastructure/Queries/Status/StatusQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace Kazue.Infrastructure.Queries.Status;
+
+public class StatusQuery
+{
+    public const string STATUS_SP_CREATE = "STATUS_SP_CREATE";
+
+    public const string STATUS_SP_DELETE = "STATUS_SP_DELETE";
+
+    public const string STATUS_SP_GET = "STATUS_SP_GET";
+
+    public const string STATUS_SP_GET_BY_ID = "STATUS_SP_GET_BY_ID";
+
+    public const string STATUS_SP_UPDATE = "STATUS_SP_UPDATE";
+}
