8d91e2d9962a15db7b789cfde9f16cebdd942e94
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:05:51 -0300

    feat: updating shared responses

---
+++ src/Domain/Kazue.Domain/Response/Shared/ItemResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Kazue.Domain.Response.Shared;
+
+public class ItemResponse
+{
+    public long Id { get; set; }
+    public string Code { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
