f372fc6ee859b8d8d758f42de87291b3ac73cac5
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:10:04 -0300

    feat: status adapter

---
+++ src/Application/Kazue.Application/Adapter/Status/StatusAdapter.cs
@@ -0,0 +1,20 @@
+﻿using Kazue.Domain.Entities.Status;
+using Kazue.Domain.Response.Status;
+
+namespace Kazue.Application.Adapter.Status;
+
+public static class StatusAdapter
+{
+    public static StatusResponse FromEntityToResponse(StatusEntity? entity)
+    {
+        if (entity is null)
+            throw new ArgumentNullException("Cannot adapt a null entity to response", nameof(entity));
+
+        return new StatusResponse
+        {
+            Id = entity.ID_STATUS,
+            Code = entity.CD_STATUS,
+            Description = entity.DS_STATUS
+        };
+    }
+}
