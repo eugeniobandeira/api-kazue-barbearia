7c6413a9da91ce68a5f1dc152f0b741422078822
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:02:55 -0300

    feat: status entity

---
+++ src/Domain/Kazue.Domain/Entities/Status/StatusEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Kazue.Domain.Entities.Status;
+
+public class StatusEntity
+{
+    public long ID_STATUS { get; set; }
+    public string CD_STATUS { get; set; } = string.Empty;
+    public string DS_STATUS { get; set; } = string.Empty;
+}
