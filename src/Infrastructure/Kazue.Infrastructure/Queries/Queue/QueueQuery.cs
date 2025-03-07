32059a79c0851eec3ebf6913575f9f49868f62a8
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:07:46 -0300

    feat: queue query

---
+++ src/Infrastructure/Kazue.Infrastructure/Queries/Queue/QueueQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace Kazue.Infrastructure.Queries.Queue;
+
+public class QueueQuery
+{
+    public const string QUEUE_SP_CREATE = "QUEUE_SP_CREATE";
+
+    public const string QUEUE_SP_DELETE = "QUEUE_SP_DELETE";
+
+    public const string QUEUE_SP_GET = "QUEUE_SP_GET";
+
+    public const string QUEUE_SP_GET_BY_ID = "QUEUE_SP_GET_BY_ID";
+
+    public const string QUEUE_SP_UPDATE = "QUEUE_SP_UPDATE";
+}
