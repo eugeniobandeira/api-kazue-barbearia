eb73f3a84ef86ba075b024bc20062b59eaff6d22
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:08:29 -0300

    feat: service repository

---
+++ src/Infrastructure/Kazue.Infrastructure/Repository/Service/ServiceRepository.cs
@@ -0,0 +1,42 @@
+﻿using Kazue.Domain.Entities.Service;
+using Kazue.Domain.Interfaces.Infrastructure.Repository.Service;
+using Kazue.Domain.Request.Service;
+
+namespace Kazue.Infrastructure.Repository.Service;
+
+public class ServiceRepository :
+    ICreateServiceRepository,
+    IReadServiceRepository,
+    IDeleteServiceRepository,
+    IUpdateServiceRepository
+{
+    public Task<ServiceEntity> Create(ServiceRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceEntity?> GetById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceEntity?> GetByCodeOrDescription(ServiceRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ServiceEntity>> GetAll(GetServiceRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(ServiceEntity serviceEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(long id, ServiceRequest req)
+    {
+        throw new NotImplementedException();
+    }
+}
