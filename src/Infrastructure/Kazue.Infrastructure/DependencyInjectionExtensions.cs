4e198113d24fdafcca86518fdfe7f1a6fdf67391
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:09:23 -0300

    feat: update infrastructure dependency injection

--- src/Infrastructure/Kazue.Infrastructure/DependencyInjectionExtensions.cs
+++ src/Infrastructure/Kazue.Infrastructure/DependencyInjectionExtensions.cs
@@ -1,10 +1,18 @@
 ﻿using Kazue.Domain.Interfaces.Infrastructure.Connection;
+using Kazue.Domain.Interfaces.Infrastructure.Repository.Queue;
+using Kazue.Domain.Interfaces.Infrastructure.Repository.Service;
+using Kazue.Domain.Interfaces.Infrastructure.Repository.Status;
 using Kazue.Domain.Interfaces.Infrastructure.Repository.User;
 using Kazue.Domain.Interfaces.Security.Cryptography;
 using Kazue.Domain.Interfaces.Security.Token;
 using Kazue.Domain.Interfaces.Service.LoggedUser;
 using Kazue.Infrastructure.Connection;
+using Kazue.Infrastructure.Parameters.Service;
+using Kazue.Infrastructure.Parameters.Status;
 using Kazue.Infrastructure.Parameters.User;
+using Kazue.Infrastructure.Repository.Queue;
+using Kazue.Infrastructure.Repository.Service;
+using Kazue.Infrastructure.Repository.Status;
 using Kazue.Infrastructure.Repository.User;
 using Kazue.Infrastructure.Security.Cryptography;
 using Kazue.Infrastructure.Security.Token;
@@ -23,21 +31,7 @@ public static class DependencyInjectionExtensions
         AddRepositories(services);
         AddParameters(services);
     }
-
-    private static void AddRepositories(IServiceCollection services)
-    {
-        services.AddScoped<ICreateUserRepository, UserRepository>();
-        services.AddScoped<IReadUserRepository, UserRepository>();
-        services.AddScoped<IUpdateUserRepository, UserRepository>();
-        services.AddScoped<IDeleteUserRepository, UserRepository>();
-    }
-
-    private static void AddParameters(IServiceCollection services)
-    {
-        services.AddScoped<IKazueConnection, KazueConnection>();
-        services.AddScoped<IUserParameter, UserParameters>();
-    }
-
+    
     private static void AddToken(IServiceCollection services, IConfiguration configuration)
     {
         var expirationTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpiresMinutes");
@@ -51,5 +45,36 @@ public static class DependencyInjectionExtensions
     {
         services.AddScoped<IPasswordEncrypter, PasswordEncrypter>();
         services.AddScoped<ILoggedUser, LoggedUser>();
+    }
+
+    private static void AddParameters(IServiceCollection services)
+    {
+        services.AddScoped<IKazueConnection, KazueConnection>();
+        services.AddScoped<IUserParameter, UserParameters>();
+        services.AddScoped<IStatusParameter, StatusParameter>();
+        services.AddScoped<IServiceParameter, ServiceParameter>();
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<ICreateUserRepository, UserRepository>();
+        services.AddScoped<IReadUserRepository, UserRepository>();
+        services.AddScoped<IUpdateUserRepository, UserRepository>();
+        services.AddScoped<IDeleteUserRepository, UserRepository>();
+
+        services.AddScoped<ICreateStatusRepository, StatusRepository>();
+        services.AddScoped<IReadStatusRepository, StatusRepository>();
+        services.AddScoped<IDeleteStatusRepository, StatusRepository>();
+        services.AddScoped<IUpdateStatusRepository, StatusRepository>();
+
+        services.AddScoped<ICreateServiceRepository, ServiceRepository>();
+        services.AddScoped<IDeleteServiceRepository, ServiceRepository>();
+        services.AddScoped<IReadServiceRepository, ServiceRepository>();
+        services.AddScoped<IUpdateServiceRepository, ServiceRepository>();
+
+        services.AddScoped<ICreateQueueRepository, QueueRepository>();
+        services.AddScoped<IDeleteQueueRepository, QueueRepository>();
+        services.AddScoped<IReadQueueRepository, QueueRepository>();
+        services.AddScoped<IUpdateQueueRepository, QueueRepository>();
     }
 }
