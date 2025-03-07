b71f23b9614daa33877450522acbb1919dd564b3
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:09:55 -0300

    feat: service adapter

---
+++ src/Application/Kazue.Application/Adapter/Service/ServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using Kazue.Domain.Entities.Service;
+using Kazue.Domain.Response.Service;
+
+namespace Kazue.Application.Adapter.Service;
+
+public static class ServiceAdapter
+{
+    public static ServiceResponse FromEntityToResponse(ServiceEntity? entity)
+    {
+        if (entity is null)
+            throw new ArgumentNullException("Cannot adapt a null entity to response", nameof(entity));
+
+        return new ServiceResponse
+        {
+            Id = entity.ID_SERVICE,
+            Code = entity.CD_SERVICE,
+            Description = entity.DS_SERVICE,
+            Price = entity.VL_PRICE
+        };
+    }
+}
