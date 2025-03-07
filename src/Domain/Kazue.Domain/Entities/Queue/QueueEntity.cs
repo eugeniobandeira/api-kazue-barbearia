5185f908a22075b47bb7aeb9c4c0df2da1d5ad36
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:02:13 -0300

    feat: queue entity

---
+++ src/Domain/Kazue.Domain/Entities/Queue/QueueEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Kazue.Domain.Entities.Queue;
+
+public class QueueEntity
+{
+    public long ID_QUEUE { get; set; }
+    public DateTime DT_CHECKIN_AT { get; set; }
+    public DateTime? DT_CHECKOUT_AT { get; set; }
+    public decimal VL_AMOUNT { get; set; }
+
+    public long ID_CUSTOMER { get; set; }
+    public string DS_CUSTOMER_NAME { get; set; } = string.Empty;
+    public string DS_CUSTOMER_SURNAME { get; set; } = string.Empty;
+    public string? DS_CUSTOMER_NICKNAME { get; set; } = string.Empty;
+
+    public long ID_BARBER { get; set; }
+    public string DS_BARBER_NAME { get; set; } = string.Empty;
+    public string DS_BARBER_SURNAME { get; set; } = string.Empty;
+    public string? DS_BARBER_NICKNAME { get; set; } = string.Empty;
+
+    public long ID_STATUS { get; set; }
+    public string CD_STATUS { get; set; } = string.Empty;
+    public string DS_STATUS { get; set; } = string.Empty;
+
+    public string IDS_SERVICES { get; set; } = string.Empty;
+}
