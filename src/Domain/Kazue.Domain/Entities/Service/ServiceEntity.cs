560055e6bde0d72f79558cc47da923ce46634bf7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:02:39 -0300

    feat: service entity

---
+++ src/Domain/Kazue.Domain/Entities/Service/ServiceEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Kazue.Domain.Entities.Service;
+
+public class ServiceEntity
+{
+    public long ID_SERVICE { get; set; }
+    public string CD_SERVICE { get; set; } = string.Empty;
+    public string DS_SERVICE { get; set; } = string.Empty;
+    public decimal VL_PRICE { get; set; }
+}
