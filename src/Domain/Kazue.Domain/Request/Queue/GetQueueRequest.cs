b1ec09b1727ae5cfd5fd967bb7554a8e47204704
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:04:31 -0300

    feat: queue request

---
+++ src/Domain/Kazue.Domain/Request/Queue/GetQueueRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kazue.Domain.Request.Queue;
+
+public class GetQueueRequest
+{
+    public long IdCustomer { get; set; }
+    public long IdBarber { get; set; }
+    public long CdService { get; set; }
+
+    [Required]
+    public int Page { get; set; }
+
+    [Required]
+    public int PageSize { get; set; }
+}
---
+++ src/Domain/Kazue.Domain/Request/Queue/QueueRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Kazue.Domain.Request.Queue;
+
+public class QueueRequest
+{
+    public long IdCustomer { get; set; }
+    public long IdBarber { get; set; }
+    public List<string> IdServices { get; set; } = [];
+}
