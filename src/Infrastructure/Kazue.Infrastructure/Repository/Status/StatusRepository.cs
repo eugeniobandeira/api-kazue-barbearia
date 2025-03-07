e916d11f3e9896ef7495a2a1933abee97dcb17ff
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:08:37 -0300

    feat: status repository

---
+++ src/Infrastructure/Kazue.Infrastructure/Repository/Status/StatusRepository.cs
@@ -0,0 +1,42 @@
+﻿using Kazue.Domain.Entities.Status;
+using Kazue.Domain.Interfaces.Infrastructure.Repository.Status;
+using Kazue.Domain.Request.Status;
+
+namespace Kazue.Infrastructure.Repository.Status;
+
+public class StatusRepository :
+    ICreateStatusRepository,
+    IReadStatusRepository,
+    IUpdateStatusRepository,
+    IDeleteStatusRepository
+{
+    public Task<StatusEntity> Create(StatusRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StatusEntity?> GetById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StatusEntity?> GetByCodeOrDescription(StatusRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<StatusEntity>> GetAll(GetStatusRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(StatusEntity statusEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(long id, StatusRequest req)
+    {
+        throw new NotImplementedException();
+    }
+}
