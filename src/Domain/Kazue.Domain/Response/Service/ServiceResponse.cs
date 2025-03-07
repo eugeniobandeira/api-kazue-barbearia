eef11cb7a4fb938b447ea2453a0ef586926ff308
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:05:35 -0300

    feat: service response

---
+++ src/Domain/Kazue.Domain/Response/Service/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Kazue.Domain.Response.Service;
+
+public class ServiceResponse
+{
+    public long Id { get; set; }
+    public string Code { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
