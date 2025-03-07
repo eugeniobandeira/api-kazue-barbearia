6d901105a4111dd3143d985429a10712db323bb7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:06:19 -0300

    feat: update user response

---
+++ src/Domain/Kazue.Domain/Response/User/ShortUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Kazue.Domain.Response.User;
+
+public class ShortUserResponse
+{
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string NickName { get; set; } = string.Empty;
+}
---
+++ src/Domain/Kazue.Domain/Response/User/UserProfileResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Kazue.Domain.Response.User;
+
+public class UserProfileResponse
+{
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Nickname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
