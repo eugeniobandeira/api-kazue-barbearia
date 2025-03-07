cfcd25fd9c9fbe6d1122a2426c2112d6201f3d37
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:12:00 -0300

    feat: update application dependency injection

--- src/Application/Kazue.Application/DependencyInjectionExtension.cs
+++ src/Application/Kazue.Application/DependencyInjectionExtension.cs
@@ -1,6 +1,20 @@
 ﻿using Kazue.Application.UseCases.Login.DoLogin;
+using Kazue.Application.UseCases.Service.Create;
+using Kazue.Application.UseCases.Service.Delete;
+using Kazue.Application.UseCases.Service.Get;
+using Kazue.Application.UseCases.Service.GetById;
+using Kazue.Application.UseCases.Service.Update;
+using Kazue.Application.UseCases.Status.Create;
+using Kazue.Application.UseCases.Status.Delete;
+using Kazue.Application.UseCases.Status.Get;
+using Kazue.Application.UseCases.Status.GetById;
+using Kazue.Application.UseCases.Status.Update;
+using Kazue.Application.UseCases.User.ChangePassword;
 using Kazue.Application.UseCases.User.Create;
+using Kazue.Application.UseCases.User.Delete;
+using Kazue.Application.UseCases.User.Get;
 using Kazue.Application.UseCases.User.GetById;
+using Kazue.Application.UseCases.User.Update;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Kazue.Application;
@@ -14,9 +28,26 @@ public static class DependencyInjectionExtension
 
     private static void AddUseCases(IServiceCollection services)
     {
-        services.AddScoped<ICreateUserUseCase, CreateUserUseCase>();
-        services.AddScoped<IGetUserByIdUseCase, GetUserByIdUseCase>();
         services.AddScoped<IDoLoginUseCase, DoLoginUseCase>();
+
+        services.AddScoped<ICreateUserUseCase, CreateUserUseCase>();
+        services.AddScoped<IChangePasswordUserUseCase, ChangePasswordUserUseCase>();
+        services.AddScoped<IDeleteUserUseCase, DeleteUserUseCase>();
+        services.AddScoped<IGetUserUseCase, GetUserUseCase>();
+        services.AddScoped<IGetUserByIdUseCase, GetUserByIdUseCase>();
+        services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
+
+        services.AddScoped<ICreateStatusUseCase, CreateStatusUseCase>();
+        services.AddScoped<IDeleteStatusUseCase, DeleteStatusUseCase>();
+        services.AddScoped<IGetStatusUseCase, GetStatusUseCase>();
+        services.AddScoped<IGetStatusByIdUseCase, GetStatusByIdUseCase>();
+        services.AddScoped<IUpdateStatusUseCase, UpdateStatusUseCase>();
+
+        services.AddScoped<ICreateServiceUseCase, CreateServiceUseCase>();
+        services.AddScoped<IDeleteServiceUseCase, DeleteServiceUseCase>();
+        services.AddScoped<IGetServiceUseCase, GetServiceUseCase>();
+        services.AddScoped<IGetServiceByIdUseCase, GetServiceByIdUseCase>();
+        services.AddScoped<IUpdateServiceUseCase, UpdateServiceUseCase>();
     }
 
 }
