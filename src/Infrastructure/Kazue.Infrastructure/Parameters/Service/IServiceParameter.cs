db0af7663285b392f62308cd76b8d34e12045c74
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:07:07 -0300

    feat: service parameter

---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Service/IServiceParameter.cs
@@ -0,0 +1,11 @@
+﻿using Kazue.Domain.Request.Service;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Service;
+
+public interface IServiceParameter
+{
+    MySqlDynamicParameters CreateParameters(ServiceRequest req);
+    MySqlDynamicParameters UpdateParameters(ServiceRequest req);
+    MySqlDynamicParameters GetByIdParameters(long id);
+}
---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Service/ServiceParameter.cs
@@ -0,0 +1,22 @@
+﻿using Kazue.Domain.Request.Service;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Service;
+
+public class ServiceParameter : IServiceParameter
+{
+    public MySqlDynamicParameters CreateParameters(ServiceRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MySqlDynamicParameters GetByIdParameters(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MySqlDynamicParameters UpdateParameters(ServiceRequest req)
+    {
+        throw new NotImplementedException();
+    }
+}
