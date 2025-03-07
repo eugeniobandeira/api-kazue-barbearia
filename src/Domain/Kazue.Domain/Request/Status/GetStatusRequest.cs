7ab8d1a350d4397fb9777bb95a2faeddfcfb5064
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:04:53 -0300

    feat: status request

---
+++ src/Domain/Kazue.Domain/Request/Status/GetStatusRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kazue.Domain.Request.Status;
+
+public class GetStatusRequest
+{
+    public string? Code { get; set; }
+    public string? Description { get; set; } 
+
+    [Required]
+    public int Page { get; set; }
+
+    [Required]
+    public int PageSize { get; set; }
+}
---
+++ src/Domain/Kazue.Domain/Request/Status/StatusRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Kazue.Domain.Request.Status;
+
+public class StatusRequest
+{
+    public string Code { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
