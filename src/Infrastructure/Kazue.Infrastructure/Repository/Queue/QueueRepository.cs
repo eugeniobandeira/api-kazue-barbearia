9ba21d41af4e001cc9b630a08f0b9918b70f3e8d
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:08:13 -0300

    feat: queue repository

---
+++ src/Infrastructure/Kazue.Infrastructure/Repository/Queue/QueueRepository.cs
@@ -0,0 +1,42 @@
+﻿using Kazue.Domain.Entities.Queue;
+using Kazue.Domain.Interfaces.Infrastructure.Repository.Queue;
+using Kazue.Domain.Request.Queue;
+
+namespace Kazue.Infrastructure.Repository.Queue;
+
+public class QueueRepository :
+    ICreateQueueRepository,
+    IDeleteQueueRepository,
+    IReadQueueRepository,
+    IUpdateQueueRepository
+{
+    public Task UpdateAsync(long id, QueueRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<QueueEntity> CreateAsync(QueueRequest queueRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(QueueEntity queueEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<QueueEntity?> GetById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<QueueEntity?> GetByIdUser(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<QueueEntity>> GetAll(GetQueueRequest req)
+    {
+        throw new NotImplementedException();
+    }
+}
