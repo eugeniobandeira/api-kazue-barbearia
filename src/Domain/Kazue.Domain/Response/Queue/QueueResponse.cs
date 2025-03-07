4bed536101b77e5d001d876d3b8e943021b89750
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:05:22 -0300

    feat: queue response

---
+++ src/Domain/Kazue.Domain/Response/Queue/QueueResponse.cs
@@ -0,0 +1,17 @@
+﻿using Kazue.Domain.Response.Service;
+using Kazue.Domain.Response.Shared;
+using Kazue.Domain.Response.User;
+
+namespace Kazue.Domain.Response.Queue;
+
+public class QueueResponse
+{
+    public long Id { get; set; }
+    public DateTime DtCheckinAt { get; set; }
+    public DateTime? DtCheckoutAt { get; set; }
+    public decimal Amount { get; set; }
+    public ShortUserResponse Customer { get; set; }
+    public ShortUserResponse Barber { get; set; }
+    public ItemResponse Status { get; set; }
+    public List<ServiceResponse> Services { get; set; }
+}
