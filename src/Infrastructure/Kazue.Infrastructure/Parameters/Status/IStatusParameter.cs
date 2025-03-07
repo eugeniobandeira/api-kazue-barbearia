fa7ea2f184fc34cf37debdc889048dc14166365f
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-03-07 17:07:18 -0300

    feat: status parameter

---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Status/IStatusParameter.cs
@@ -0,0 +1,11 @@
+﻿using Kazue.Domain.Request.Status;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Status;
+
+public interface IStatusParameter
+{
+    MySqlDynamicParameters CreateParameters(StatusRequest req);
+    MySqlDynamicParameters UpdateParameters(StatusRequest req);
+    MySqlDynamicParameters GetByIdParameters(long id);
+}
---
+++ src/Infrastructure/Kazue.Infrastructure/Parameters/Status/StatusParameter.cs
@@ -0,0 +1,22 @@
+﻿using Kazue.Domain.Request.Status;
+using Kazue.Infrastructure.Helpers;
+
+namespace Kazue.Infrastructure.Parameters.Status;
+
+public class StatusParameter : IStatusParameter
+{
+    public MySqlDynamicParameters CreateParameters(StatusRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MySqlDynamicParameters UpdateParameters(StatusRequest req)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MySqlDynamicParameters GetByIdParameters(long id)
+    {
+        throw new NotImplementedException();
+    }
+}
